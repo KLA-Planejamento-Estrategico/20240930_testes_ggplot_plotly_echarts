1e1b1e1ae5c8ac7b46686f6771d57c2a35a9a1c1
viallemos <flavialemosfv@gmail.com>
2024-11-04 15:01:34 -0300

    pós follow

--- 03_scripts/13_testes_setimo_follow_reprex.R
+++ 03_scripts/13_testes_setimo_follow_reprex.R
@@ -8,30 +8,23 @@ cinco_cores_pin <- c("#E0F7E0", "#B2E1B2", "#80D080", "#4CAF50", "#009900")
 tres_cores_euc <- c("#FFA500", "#FF8C00", "#FF4500" )
 cinco_cores_euc <- c("#FFF3E0", "#FFE0B2", "#FFCC80", "#FF9800", "#FF5722")
 
-# carregando os dados
-data <- readxl::read_xlsx("01_raw_data/teste_segundo_grafico_fechado.xlsx") |>
-    janitor::clean_names()
 
-# Gravação
-camcorder::gg_record(
-    dir = "recording_dir", # diretório p salvar as imagens
-    device = "png", # formato da imagem
-    width = 20, # largura
-    height = 7, # altura
-    dpi = 300, # resolução da imagem
-    bg = "white" # fundo branco
-)
+data_past_tribble <- tibble::tribble(
+                                   ~classe, ~`2020`, ~`2021`, ~`2022`, ~`2023`, ~`2024`, ~`2025`, ~`2026`, ~`2027`, ~`2028`, ~`2029`, ~`2030`, ~`2031`, ~`2032`, ~`2033`, ~`2034`, ~`2035`, ~`2036`, ~`2037`, ~`2038`, ~`2039`,
+        "Estoque de Euc Processo - 6 anos", 147.697,  27.838, 199.565,   94.19, 143.085,      0L,   2.487,      0L,  49.412, 169.909, 383.006, 448.102,  40.477,   9.248, 190.582,   4.231,  119.65, 193.039, 175.005, 135.927,
+        "Estoque de Euc Processo - 7 anos", 107.565, 174.907,  32.841, 171.273,   55.08,      0L,       0,      0L,       0,  52.007,  57.853, 258.981,  334.34,  37.381,   1.359, 219.378,       0, 119.898, 109.159,  21.516,
+    "Estoque de Euc Processo - 8_100 anos", 345.392, 411.085, 302.369,  77.763,       0,      0L,       0,      0L,       0,       0,  57.928,  82.607, 195.349,  410.81, 427.986, 458.358, 465.486, 489.664, 511.087, 530.155
+    ) |> janitor::clean_names()
+
 
 # transformando os dados
-data_long <- data |>
+data_long <- data_past_tribble |>
     tidyr::pivot_longer(
         cols = starts_with("x20"), names_to = "ano", values_to = "volume"
     ) |>
     dplyr::mutate(
-        ano = stringr::str_remove_all(ano, pattern = "x"),
-        volume = volume / 10^3
+        ano = stringr::str_remove_all(ano, pattern = "x")
     ) |>
-    dplyr::rename(classe = x1) |>
     dplyr::mutate(
         classe = dplyr::case_when(
             classe == "Estoque de Euc Processo - 6 anos" ~ "6 anos",
@@ -57,60 +50,32 @@ data_long_fator <- data_long |>
 
 # vetor de cores nomeado
 cores <- tres_cores_pin
-# cores <- unique(data_long_fator$cor) |> sort() # ex. de como seria com coluna com cores puxadas do vetor
 classes <- unique(data_long_fator$classe) # |> sort() # mesma coisa q arrange só q por vetor
 names(cores) <- classes
 data_long_fator$cor <- cores[data_long_fator$classe]
-
-# calcular os totais empilhados
-totais <- data_long_fator |>
-    dplyr::group_by(ano) |>
-    dplyr::summarize(total_volume = sum(volume), .groups = 'drop')
 
 max_y <- max(data_long_fator$total_volume, na.rm = TRUE) |> round(0)
 intervalo_y <- ceiling(max_y / 500) * 100
 # divide o valor máximo por 5 e arredonda para o múltiplo de 100 mais próximo
 
 # barras empilhadas
-d <-
-    # data_long_fator |>
-    # tibble::rownames_to_column() |> # verificar motivo
-    ggplot(
+d <- ggplot(
         data = data_long_fator,
         mapping = aes(
-        x = ano,
-        y = volume,
-        fill = classe,
-        # usando a classe para definir as cores
-        tooltip = paste("Volume:", volume, "mil m³\nIdade:", classe)
-    )) +
+            x = ano,
+            y = volume,
+            fill = classe,
+            # usando a classe para definir as cores
+            tooltip = paste("Volume:", volume, "mil m³\nIdade:", classe)
+        )) +
     geom_bar_interactive(
         stat = "identity", # não conta as obss usa como está nos dados
         position = "stack", # barras empilhadas
         color = "white", size = 1 # bordas brancas
     ) +
-    # # linha dos totais
-    # geom_line(
-    #     data = data_long_fator  |>  dplyr::distinct(ano, total_volume),
-    #     aes(x = ano, y = total_volume),
-    #     group = 1, # dados agrupados cada um como um valor por ano
-    #     color = "black",
-    #     size = 1, # espessura
-    #     linetype = "dashed" # linha pontilhada atoa
-    # ) +
-    # # rótulo dos totais
-    # geom_text(
-    #     data = data_long_fator |>  dplyr::distinct(ano, total_volume),
-    #     mapping = aes(
-    #         x = ano,
-    #         y = total_volume,
-    #         label = round(total_volume, 0),
-    #         vjust = -0.5,
-    #         color = "black"
-    #
-    #     ),
-    # ) +
-    theme( # sempre por ultimo
+
+    theme(
+        # sempre por ultimo
         text = element_text(size = 28, family = "Times New Roman"),
         panel.grid.major.y = element_line(color = "grey20", linetype = "dashed"),
         panel.grid.minor = element_blank(),
@@ -119,10 +84,16 @@ d <-
         legend.position = "top",
         legend.box = "horizontal",
         legend.title = element_blank(),
-        plot.title = element_text(hjust = 0.5, face = "bold", margin = margin(b = 30)), # aumentando a margem inferior do titulo
+        plot.title = element_text(
+            hjust = 0.5,
+            face = "bold",
+            margin = margin(b = 30)
+        ),
+        # aumentando a margem inferior do titulo
         panel.background = element_rect(fill = "white"),
         legend.key.width = unit(1.5, "cm") # legenda com cor retangular
     ) +
+
     scale_y_continuous(
         expand = c(0, 0),
         limits = c(0, intervalo_y * 5),  # define o limite de y 5 vezes o intervalo calculado
@@ -134,13 +105,11 @@ d <-
          x = "Anos",
          y = "Milhares"
     )
-d
-camcorder::gg_stop_recording()
 
 # interatividade
-girafe(
-    ggobj = d,
-    width_svg = 20,
-    height_svg = 5
-)
+# girafe(
+#     ggobj = d,
+#     width_svg = 20,
+#     height_svg = 5
+# )
 
